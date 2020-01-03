60d8955f894a94470e465e5e0c5b5420b9ec1a7c
slzander <staceyzander@gmail.com>
2020-01-02 22:32:43 -0700

    adds update action to bird controller, brings in mappedBirds params and maps through them to update on_map column in table

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,4 +3,15 @@ class BirdsController < ApplicationController
         birds = Bird.all 
         render json: birds, include:[:locations]
     end
+
+    def update
+        mappedBirds = params[:mappedBirds]
+        Bird.all.map do |bird|
+            if mappedBirds.include?(bird.id)
+               bird.update(on_map: true)
+            else
+                bird.update(on_map: false)
+            end
+        end
+    end
 end
